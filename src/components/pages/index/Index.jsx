138e13a0ecddd723666c8443defb5e2cd2d1e157
Nebeolisa chiemezie Samson <daddygreenarrow44@gmail.com>
2023-11-25 20:47:18 +0900

    done with the front page main about to work on footer

--- src/components/pages/index/Index.jsx
+++ src/components/pages/index/Index.jsx
@@ -1,28 +1,94 @@
 import React from 'react'
 import logo from "./../../../assets/images/logo.png"
+import { EventList } from '../../../data'
+import { Button } from '../../sharedComponents/SharedComponents'
 
 const Index = () => {
   return (
-    <article>
-      {/* ==== Hero section === */}
-      <section className='border-4 border-solid border-teal-500 rounded-xl p-12 text-white'>
-        <h2 className='text-7xl text-green-500 font-lora uppercase italic font-extrabold tracking-tighter'>don't miss out your battle</h2>
+    <article className='text-white'>
 
-        <div className='grid grid-cols-2 items-center'>
-          <div>
-            <p>
+      {/* ==== Hero section === */}
+      <section className='border-4 border-solid border-teal-500 rounded-xl pt-10 px-4 my-20'>
+        <h2 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-green-500 font-lora uppercase italic font-extrabold tracking-tighter'>don't miss out your battle</h2>
+
+        <div className='grid md:grid-cols-2 items-center my-6 md:mb-0 md:mt-0'>
+          <div className='md:p-2 order-last md:order-first'>
+            <p className='text-sm 2xl:text-lg font-round sm:text-base'>
               Welcome to Find your battle, your ultimate source for breaking battle updates in Korea and beyond! We're dedicated to keeping 
               breaking enthusiasts informed about upcoming B-boy battles, jams, cyphers, and championships. You'll never miss a beat, staying
               on top of the freshest breaking events happening in your area.
             </p>
-            <button></button>
+            <button className='mt-10 bg-teal-500 rounded-md px-2 py-1.5 hover:bg-teal-400 transition capitalize'>
+              learn more
+            </button>
           </div>
 
+          <div className='order-first md:order-last'>
+            <img src={logo} alt="logo" className='max-h-72 max-w-[288px] mx-auto md:max-h-full md:max-w-full'/>
+          </div>
+        </div>       
+      </section>
+
+      {/* ==== Featured Battles ==== */}
+      <section className='py-20'>
+        <div className='max-w-4xl mx-auto text-center mt-10 mb-20'>
+          <h2 className='text-xl sm:text-3xl text-green-500 font-lora uppercase italic font-extrabold tracking-tighter mb-6'>FEATURED BATTLES</h2>
+          <p className='sm:text-lg lg:text-2xl font-round'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+          </p>
+        </div>
+
+        <div className='w-full overflow-scroll md:overflow-visible'>
+          <ul className='text-stone-800 font-poppins bg-white px-10 py-7 min-w-[768px]'>
+            {EventList?.map((event) => {
+              return (
+                <li key={event.id} className="even:bg-[#e2e2e2] group py-2 px-2 rounded capitalize cursor-pointer text-sm lg:text-base grid grid-cols-6 gap-x-4 first:font-semibold first:text-lg first:font-round">
+                  <div className='col-span-1'> 
+                    {event.date}
+                  </div>
+
+                  <div className='col-span-2'>
+                    {event.name}
+                  </div>    
+
+                  <div className='col-span-1'>
+                    {event.location}
+                  </div> 
+
+                  <div className='col-span-1'>
+                    {event.Genre}
+                  </div>   
+
+                  <div className='col-span-1'>
+                    {event.Format}
+                  </div>    
+
+                  {/* <span className='text-sm text-stone-400 italic capitalize hidden group-hover:block'>{event.name}</span>                              */}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+        <div className='text-center mt-24'>
+          <Button color={"bg-teal-500"} text={"Browse battles"}/>
+        </div>
+      </section>
+
+      {/* ==== Join Today/ Contact === */}
+      <section className='py-16 lg:grid lg:grid-flow-col lg:justify-between mx-auto text-center lg:text-left'>
+        <p className='sm:text-2xl text-lg font-round col-span-2 max-w-2xl mx-auto pr-2.5 lg:mx-0'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        </p>
+
+        <div className='col-span-1 grid grid-cols-2 gap-4 max-w-xs mx-auto mt-12 lg:mt-0'>
           <div>
-            <img src={logo} alt="logo" />
+            <Button color={"bg-teal-500"} text={"join today"}/>
+          </div>
+          <div>
+            <Button color={"text-teal-500 bg-white hover:text-white"} text={"contact us"}/>
           </div>
         </div>
-       
       </section>
     </article>
   )
--- src/data.js
+++ src/data.js
@@ -25,3 +25,54 @@ export const Navigations = [
 		url: "contact",
 	},
 ];
+
+export const EventList = [
+	{
+		id: 0,
+		date: "date",
+		name: "name",
+		location: "location",
+		Genre: "Genre",
+		Format: "Format",
+	},
+	{
+		id: 1,
+		date: 2023 - 11 - 26,
+		name: "Locking japan Crew battle(hyper link to Detail Page)",
+		location: "Tyokyo, japan",
+		Genre: "Locking",
+		Format: "Crew battle",
+	},
+	{
+		id: 2,
+		date: 2023 - 11 - 25,
+		name: "Break seoul 1 on 1 (hyper link to detail page)",
+		location: "Seoul, Korea",
+		Genre: "Breaking",
+		Format: "1 vs 1",
+	},
+	{
+		id: 3,
+		date: 2023 - 11 - 26,
+		name: "Locking japan Crew battle(hyper link to Detail Page)",
+		location: "Tyokyo, japan",
+		Genre: "Locking",
+		Format: "Crew battle",
+	},
+	{
+		id: 4,
+		date: 2023 - 11 - 25,
+		name: "Break seoul 1 on 1 (hyper link to detail page)",
+		location: "Seoul, Korea",
+		Genre: "Breaking",
+		Format: "1 vs 1",
+	},
+	{
+		id: 5,
+		date: 2023 - 11 - 26,
+		name: "Locking japan Crew battle(hyper link to Detail Page)",
+		location: "Tyokyo, japan",
+		Genre: "Locking",
+		Format: "Crew battle",
+	},
+];
