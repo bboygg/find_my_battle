9c40372f762ed44ba65c3477a12e6566d89485f8
Nebeolisa chiemezie Samson <daddygreenarrow44@gmail.com>
2023-11-25 22:18:00 +0900

    complete the About page

--- frontend/src/components/pages/About/About.jsx
+++ frontend/src/components/pages/About/About.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      
+    <div className='grid items-center h-[75vh]'>
+      <div className='text-white max-w-4xl mx-auto'>
+        <h2 className='text-green-500 font-lora uppercase italic font-extrabold tracking-tighter mb-16 text-center text-4xl'>About</h2>
+        <p className='text-lg lg:text-2xl font-round text-center'>
+          Find My Battle is your ultimate source for breaking battle updates in Korea and beyond! We're dedicated to keeping breaking enthusiasts 
+          informed about upcoming B-boy battles, jams, cyphers, and championships.  With Find My Battle you'll never miss a beat, staying on top 
+          of the freshest breaking events happening in your area. Not just limited to Korea, we have bold plans to expand globally, connecting 
+          breaking communities worldwide. For event organizers, our platform provides tools to manage dates seamlessly, ensuring every battle is 
+          a success. Join Find My Batle now and be part of the breaking revolution—where the future of breaking starts, and borders are meant 
+          to be broken!
+        </p>
+      </div>      
     </div>
   )
 }
